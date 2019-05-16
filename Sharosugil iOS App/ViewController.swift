d4ed6996299650cdb72a79e0b75892d24e8527ff
Jaeho Lee <jaeho.lee@snu.ac.kr>
2019-05-17 01:35:51 +0900

    :tada: Initial commit

---
+++ Sharosugil iOS App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sharosugil iOS App
+//
+//  Created by Jay Lee on 17/05/2019.
+//  Copyright © 2019 Sharosugil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
